d8dff011b76725f717beba251030024c68471869
SRAB <samblakeman@sams-macbook-pro-3.local>
2017-05-01 22:28:58 +0100

    Initial Commit

---
+++ MachineLearningLibrary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MachineLearningLibrary
+//
+//  Created by Sam Blakeman on 01/05/2017.
+//  Copyright © 2017 Sam Blakeman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
